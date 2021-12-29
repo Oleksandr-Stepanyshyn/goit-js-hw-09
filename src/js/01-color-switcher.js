6db134946710276622e539693b853f8d40c1102e
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2021-12-29 19:20:46 +0200

    finish task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    btnStart: document.querySelector("button[data-start]"),
+    btnStop: document.querySelector("button[data-stop]")
+}
 
+refs.btnStop.setAttribute('disabled', true);
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onButtonStartClick);
+refs.btnStop.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+    refs.btnStart.setAttribute('disabled', true);
+    refs.btnStop.removeAttribute('disabled');
+    timerId = setInterval(renderBackColorBody, 1000);
+}
+
+function onButtonStopClick() {
+    refs.btnStop.setAttribute('disabled', true);
+    refs.btnStart.removeAttribute('disabled');
+    clearInterval(timerId);
+}
+
+function renderBackColorBody() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
